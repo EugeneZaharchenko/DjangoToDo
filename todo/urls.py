e226e7939ddabd4d05ed1bb969934303fa0d8024
eugen <eugenezaharchenko@gmail.com>
2018-10-05 07:57:37 +0300

    Add добавлено, верстка. Основной функционал сделан

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('main/', views.main, name='main'),
+    path('details/<int:id>/', views.details),
+    path('add/', views.add, name='add'),
 ]
