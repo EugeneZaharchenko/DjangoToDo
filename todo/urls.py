e61b2d743969b7b8d6e92d8fe0f71100e4c4ccc8
eugen <eugenezaharchenko@gmail.com>
2018-10-07 08:51:20 +0300

    add добавлено

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('main/', views.main, name='main'),
-    path('details/<int:id>/', views.details),
+    path('details/<int:id>', views.details),
     path('add/', views.add, name='add'),
+    path('check/<int:id>', views.check, name='check'),
+    path('uncheck/<int:id>', views.uncheck, name='uncheck'),
+    path('delete/<task_id>', views.delete, name='delete'),
 ]
